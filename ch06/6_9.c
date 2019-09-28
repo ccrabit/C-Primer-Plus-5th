94708319d6a01e5ce7e82022c950f46701d07fd4
ccrab <chenchongrab@outlook.com>
2019-09-28 11:01:49 +0800

    ch06

---
+++ ch06/6_9.c
@@ -0,0 +1,15 @@
+//编写一个程序，要求用户输入下限整数和一个上限整数，
+//然后，一次计算从下限到上限的每一个整数的平方的加和，最后显示结果。
+//程序将不断提示用户输入下限整数和上限整数并显示出答案，
+//直到用户输入的上限整数小于或等于下限整数为止。
+#include <stdio.h>
+int main(void)
+{
+    printf ("Enter lower and upper integer limits:\n");
+
+    printf ("The sum of the squares from %d to %d is %d\n");
+
+    printf ("Done.\n");
+
+    return 0;
+}
